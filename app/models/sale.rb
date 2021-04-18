863a9bb67c86ff03dd5c5b0599eb0cb9623f0867
Alfredo Peña <aalfredoplz@gmail.com>
2021-04-18 10:33:10 -0400

    scope group

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,6 @@
 class Sale < ApplicationRecord
+ 
+    scope :group_origin, -> (number){ group(:origin).where('date_time > ?', number.month.ago).order(date_time: :desc).count}
+    scope :group_mix, -> (number){ group(:mix).where('date_time > ?', number.month.ago).order(date_time: :desc).count}
+
 end
